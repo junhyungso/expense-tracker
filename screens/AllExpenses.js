63fdb80af4a007ec3e78887a313f7adf94e96b9f
junhyungso <junso5551@gmail.com>
2024-10-07 16:55:04 -0400

    initial structure

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AllExpenses = () => {
+  return (
+    <View>
+      <Text>AllExpenses</Text>
+    </View>
+  );
+};
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RecentExpenses = () => {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  );
+};
+
+export default RecentExpenses;
